e3857882b94a4cd1c5ac85a4aced7e685e95532a
Rohit Raj <rohit.raj@aalto.fi>
2019-10-26 21:32:56 +0300

    improvent of client1's test code and added some logging

--- code/mysimbdp-batchingestmanager/clientbatchingestapp/company_1_clientbatchingestapp.py
+++ code/mysimbdp-batchingestmanager/clientbatchingestapp/company_1_clientbatchingestapp.py
@@ -2,6 +2,13 @@ import pymongo
 import gridfs
 from datetime import datetime
 import os 
+import logging
+import time
+
+custom_logging_format = '%(asctime)s : [%(levelname)s] - %(message)s'
+logging.basicConfig(filename= "../../logs/company1/batchingingestapp.log" , filemode="a", level= logging.INFO, format=custom_logging_format)
+
+
 # mongodb connection strings
 #Strictly internal APIs to call and save data in MongoDB
 # it requires "gridfs" to save the file into db
@@ -19,7 +26,7 @@ def __connectToDatabase():
     return getDatabase 
      
 # remember that this client need to join the file
-def join_files(fromdir, tofile):
+def join_files(fromdir, tofile):    
     output = open(tofile, 'wb')
     parts  = os.listdir(fromdir)
     parts.sort(  )
@@ -41,15 +48,18 @@ def __connectAndInsertRow(valueToBeStored):
 
 def __insertFileIntoGridFs():
     fs = gridfs.GridFS(__connectToDatabase())
-
+    
     targetFileName = "Company_1_test_split_file"
     #if there are more than one file, then we need to join them
     if len([name for name in os.listdir(__fileDirectory) if os.path.isfile(os.path.join(__fileDirectory, name))]) > 1:
+        start = time.time()
         join_files(__fileDirectory, targetFileName)
+        end=time.time()
+        logging.info(f"TIMING: the time for joining the file was : {start-end}")
 
     for file_name in os.listdir(__fileDirectory):
-        file = open(file_name)
-        
+        start = time.time()
+        file = open(file_name)        
         id = fs.put(file)
         file.close()
         data = {
@@ -59,3 +69,8 @@ def __insertFileIntoGridFs():
             "saveTime" : datetime.utcnow()
         }
         __connectAndInsertRow(data)
+        end = time.time()
+        logging.info(f"TIMING: the time for saving the file {file_name} in the DB was : {start-end}")
+        logging.info(f"successfully processed the file {file_name}. The file ID was {id}. The size was : {os.path.getsize(__fileDirectory+file_name)}")
+
+__insertFileIntoGridFs()
