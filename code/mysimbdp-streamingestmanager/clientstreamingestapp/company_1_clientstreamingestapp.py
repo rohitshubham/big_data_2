3aa355d545c2e6a56b9ee8205a9bbcbbf40f5006
Rohit Raj <rohit.raj@aalto.fi>
2019-10-28 21:42:55 +0200

    Added reporting facility into company1

--- code/mysimbdp-streamingestmanager/clientstreamingestapp/company_1_clientstreamingestapp.py
+++ code/mysimbdp-streamingestmanager/clientstreamingestapp/company_1_clientstreamingestapp.py
@@ -17,10 +17,16 @@ broker_url = "localhost"
 broker_port = 1883
 __connectionString = "mongodb://localhost:27017"
 __databaseName = "mysimbdp-coredms"
-total_messages = 0 
+total_messages = 0
+last_reported_messages = 0 
+avg_processing_time = 0
+total_ingestion_data_size = 0
+last_time_sent = 0
+
 
 #topic-name = "big_data_" + truncate(sha256("company_1"), 5)
 topic = "topic/big_data_c2dcc"
+topic_reporting = "topic/big_data_reporting_c2dcc"
 
 client = mqtt.Client()
 client.connect(broker_url, broker_port)
@@ -46,15 +52,34 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe(topic)
     logging.info(f"Subscribed to topic {topic} on {broker_url}")
 
+
+def send_mqtt_requests(payload):
+    client = mqtt.Client()
+    client.connect(broker_url, broker_port, 60)
+    client.publish(topic_reporting, str(payload))
+    client.disconnect()
+
+#reports the data after every 60 seconds 
+def reporting_data():
+    global last_reported_messages, avg_processing_time, total_messages, last_time_sent, total_ingestion_data_size
+    if time.time() - last_time_sent > 60:
+        send_mqtt_requests([avg_processing_time, total_messages-last_reported_messages, total_ingestion_data_size])
+        total_ingestion_data_size = 0
+        avg_processing_time = 0
+        last_time_sent = time.time()        
+
 def on_message(client, userdata, msg):
     try:
-        global total_messages
+        global total_messages, avg_processing_time, total_ingestion_data_size
         logging.info(f"Received message for client_1. start the ingestion of data into the database.")
         start = time.time()
         start_ingestion(str(msg.payload.decode()))
         end = time.time()
         logging.info("DaTA SUCCESSFULLY INGESTED.")
         total_messages = total_messages + 1
+        avg_processing_time = (avg_processing_time + (end-start))/2 # We calculate the avg after every message
+        total_ingestion_data_size = total_ingestion_data_size + sys.getsizeof(str(msg.payload.decode()))
+        reporting_data()
         logging.info(f"Reporting: Total messages ingested this session:{total_messages}")
         logging.info(f"Reporting: Size of this mesage was :{sys.getsizeof(str(msg.payload.decode()))}")
         logging.info(f"Reporting: Time taken for ingestion was {end-start}")
